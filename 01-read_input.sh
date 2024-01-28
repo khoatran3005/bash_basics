5f046093d31e116c483c50f893b9a4be16af5940
Khoa Tran <khoatran3005@gmail.com>
2024-01-28 16:04:48 -0500

    first

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,11 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+# Ask the user for a filename
+echo "Please enter a filename:"
+read filename
+
+# Create an empty file with the provided filename
+touch $filename
+
+echo "File '$filename' has been created."
